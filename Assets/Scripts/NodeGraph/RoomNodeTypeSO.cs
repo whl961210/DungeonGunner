259ae1738566539185d971fb1c04a23c695dbd4d
whl961210 <whl961210@gmail.com>
2023-11-28 14:49:47 -0800

    Added room node functions to construct nodes for room

---
+++ Assets/Scripts/NodeGraph/RoomNodeTypeSO.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "RoomNodeType_", menuName = "ScriptableObjects/Dungeon/RoomNodeType")]
+public class RoomNodeTypeSO : ScriptableObject
+{
+    public string roomNodeTypeName;
+    #region Header
+    [Header("Only flag the RoomNodeTypes that you want to display in the NodeGraphEditor")]
+    #endregion
+    public bool displayInNodeGraphEditor = true;
+    #region Header
+    [Header("One type Shuold be A Corridor")]
+    #endregion
+    public bool isCorridor;
+    #region Header
+    [Header("One type Shuold be A CorridorNS")]
+    #endregion
+    public bool isCorridorNS;
+    #region Header
+    [Header("One type Shuold be A CorridorEW")]
+    #endregion
+    public bool isCorridorEW;
+    #region Header
+    [Header("One type Shuold be A Entrance")]
+    #endregion
+    public bool isEntrance;
+    #region Header
+    [Header("One type Shuold be A Basic Room")]
+    #endregion
+    public bool isBoosRoom;
+    #region Header
+    [Header("One type Shuold be None (Unassigned)")]
+    #endregion
+    public bool isNone;
+
+    #region Validation
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckEmptyString(this, nameof(roomNodeTypeName), roomNodeTypeName);
+    }
+#endif
+    #endregion
+}
---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HelperUtilities
+{
+    public static bool ValidateCheckEmptyString(Object thisObject, string fileName, string stringToCheck)
+    {
+        if (stringToCheck == "")
+        {
+            Debug.LogError(fileName + " is empty and must contain a value in the object " + thisObject.name.ToString() + ".");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static bool ValidateCheckEnumerableValues(Object thisObject, string fileName, IEnumerable enumerableToCheck)
+    {
+        bool error = false;
+        int count = 0;
+        foreach (var item in enumerableToCheck)
+        {
+            if (item == null)
+            {
+                Debug.LogError(fileName + " contains a null value in the object " + thisObject.name.ToString() + ".");
+                error = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            Debug.LogError(fileName + " is empty and must contain a value in the object " + thisObject.name.ToString() + ".");
+            error = true;
+        }
+        return error;
+    }
+}
