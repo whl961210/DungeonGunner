f14febbf5636ef8261ff6bdc825129021ad6f8e4
whl961210 <whl961210@gmail.com>
2023-12-19 15:18:49 -0800

    created doorway class

--- Assets/Scripts/Dungeon/Doorway.cs
+++ Assets/Scripts/Dungeon/Doorway.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class Doorway : MonoBehaviour
+[System.Serializable]
+public class Doorway
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vectpr2Int position;
+    public Orientation orientation;
+    public GameObject doorPrefab;
+    #region Header
+    [Header("The Upper Left Position To Start Copying From")]
+    #endregion
+    public Vector2Int doorwayStartCopyPosition;
+    #region Header
+    [Header("The width of tiles in the doorway")]
+    #endregion
+    public int doorwayCopyTileWidth;
+    #region Header
+    [Header("The height of tiles in the doorway")]
+    #endregion
+    public int doorwayCopyTileHeight;
+    [HideInInspector]
+    public bool isConnected = false;
+    [HideInInspector]
+    public bool isUnavailable = false;
 }
