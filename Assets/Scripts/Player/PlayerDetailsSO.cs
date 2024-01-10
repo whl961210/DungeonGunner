afbb162f46ac040d9521fa48b11682ee19a8e3d6
whl961210 <whl961210@gmail.com>
2024-01-09 16:10:33 -0800

    created playerDetailsSO

---
+++ Assets/Scripts/Player/PlayerDetailsSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerDetails_", menuName = "Scriptable Objects/Player/Player Details")]
+public class PlayerDetailsSO : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
