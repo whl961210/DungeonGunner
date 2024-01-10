f09241979ac0529e62a6cc7bfd1b4b98fd482d89
whl961210 <whl961210@gmail.com>
2024-01-09 16:20:09 -0800

    created Player class

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[DisallowMultipleComponent]
+#endregion REQUIRE COMPONENTS
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
